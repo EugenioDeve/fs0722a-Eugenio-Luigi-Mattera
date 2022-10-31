ca2936a0afd5ae2e4ce7aa450ba7334d71c1e584
EugenioDeve <eugeniodeveloper@outlook.com>
2022-10-31 11:59:27 +0100

    esercizio_d1 js

---
+++ m1/w2/d1/teoria/js1/teoria/script.js
@@ -0,0 +1,27 @@
+/*
+commento su più righe
+*/
+
+//commento su una riga
+
+/*
+Direttive di output
+*/
+
+
+
+
+
+
+
+
+console.log('Mostra questa scritta');
+
+
+console.log('%c Mostra questa scritta', 'color:red;background-color:blue');
+
+
+document.getElementById('test').innerHTML = '<b>Prova</b>';
+
+
+document.write('Ciao')
