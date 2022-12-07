31136910b41df22a37bf78eada8c9477feead92c
EugenioDeve <eugeniodeveloper@outlook.com>
2022-12-07 09:21:36 +0100

    teoria ed esercizio m2/w3/d2

---
+++ m2/w3/d2/teoria/moduli/moduli/Teacher.js
@@ -0,0 +1,4 @@
+import { User } from './User.js';
+export class Teacher extends User {
+    skills = [];
+}
---
+++ m2/w3/d2/teoria/moduli/moduli/User.js
@@ -0,0 +1,10 @@
+export class User {
+    name = "";
+    surname = "";
+    age = -1;
+    constructor(_name, _surname, _age) {
+        this.name = _name;
+        this.surname = _surname;
+        this.age = _age;
+    }
+}
