6c51b8b68c722d0f85926e5d90f15c94f346c95f
EugenioDeve <eugeniodeveloper@outlook.com>
2023-01-20 01:13:41 +0100

    d4-angular w3/m3

--- m3/w3/d4-angular/d4/karma.conf.js
+++ m3/w3/d4-angular/d4/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/d4'),
+      dir: require('path').join(__dirname, './coverage/d3'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- m3/w3/d4-angular/d4/src/app/app.component.spec.ts
+++ m3/w3/d4-angular/d4/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'd4'`, () => {
+  it(`should have as title 'd3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('d4');
+    expect(app.title).toEqual('d3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('d4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('d3 app is running!');
   });
 });
